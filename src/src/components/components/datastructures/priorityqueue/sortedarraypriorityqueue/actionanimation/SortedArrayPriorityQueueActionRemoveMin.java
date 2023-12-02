fedcd6d9c00cbc175efcc8fece88da7768c780d5
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-12-03 00:50:17 +0700

    Fix bug in Priority Queue

--- src/src/components/components/datastructures/priorityqueue/sortedarraypriorityqueue/actionanimation/SortedArrayPriorityQueueActionRemoveMin.java
+++ src/src/components/components/datastructures/priorityqueue/sortedarraypriorityqueue/actionanimation/SortedArrayPriorityQueueActionRemoveMin.java
@@ -31,12 +31,15 @@ public class SortedArrayPriorityQueueActionRemoveMin extends AbstractPriorityQue
         if (animationStep == 0) {
             pickUpElement();
             animationStep++;
-        } else if (animationStep == 1) {
+        } else if (animationStep <= getRootScreen().queue.size() - 1) {
+            movePanelNodeToLeft(animationStep);
+            animationStep++;
+        } else if (animationStep == getRootScreen().queue.size()) {
             returnElement();
             animationStep++;
-        } else if (animationStep == 2) {
+        } else if (animationStep == getRootScreen().queue.size() + 1) {
             solveEnd();
-            animationStep = 3;
+            animationStep = 100000;
         } else {
             animationStep = 0;
             end();
@@ -66,6 +69,28 @@ public class SortedArrayPriorityQueueActionRemoveMin extends AbstractPriorityQue
                 0,
                 ViewArrayListAction.GAP_Y + 20,
                 10, period - 10
+        );
+    }
+
+    public void movePanelNodeToLeft(int i) {
+        int i1 = i;
+        Iterator<EntryInterface<Integer, AbstractPanelPriorityQueueNode>> iterator
+                = getRootScreen().queue.iterator();
+        while (i-- > 0) iterator.next();
+        AbstractPanelPriorityQueueNode node = iterator.next().getValue();
+        getRootScreen().setDescription(
+                String.format(
+                        "[UPDATE] Update a[%d] := a[%d] = Entry(key=%d, value=%d)",
+                        i1-1, i1, node.key, node.getValue()
+                )
+        );
+        ServiceAnimation.translate(
+                node,
+                new Location(node.getX(), node.getY()),
+                -ViewArrayListAction.SIZE_PER_NODE - ViewArrayListAction.GAP_X,
+                0,
+                10,
+                period - 10
         );
     }
 
